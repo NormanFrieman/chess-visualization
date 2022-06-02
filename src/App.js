ce201539479c70e76e7dadbfd54b1b14a7c95c55
Norman Frieman <ezequiel-d.s@hotmail.com>
2022-06-02 11:45:28 -0300

    feat: indicar no tabuleiro quais movimentos são possíveis ao clicar ou arrastar uma peça

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import move_sound from './assets/move-sound.mp3';
 
 function App() {
   const [game, setGame] = useState(new Chess());
+  const [squaresStyles, setSquaresStyles] = useState({});
   const [moveSound] = useSound(move_sound);
+
+  function clearSquares() {
+    setSquaresStyles({});
+    return;
+  }
 
   function safeMove(modify) {
     setGame(g => {
@@ -16,14 +22,8 @@ function App() {
     })
   }
 
-  function test2(piece){
-    console.log(piece);
-    console.log("CLICOU");
-  }
-
   function onDrop(sourceSquare, targetSquare) {
     let move = null;
-    console.log(sourceSquare + " " + targetSquare);
     safeMove(game => {
       move = game.move({
         from: sourceSquare,
@@ -31,22 +31,50 @@ function App() {
         promotion: 'q'
       });
     })
-
     if(move === null)
       return false;
     
     moveSound();
+    clearSquares();
     return true;
+  }
+
+  function possiblesMoves(square){
+    clearSquares();
+    const squares = game.moves({ square });
+
+    const style = {};
+    squares.forEach(square => {
+      if(square.length > 2)
+        square = square.slice(square.length - 2);
+      
+      style[square] = game.square_color(square) === "light" ? 
+        { backgroundColor: 'rgb(0,114,23,0.1)' } :
+        { backgroundColor: 'rgb(0,114,23,0.3)' };
+    });
+    setSquaresStyles(style);
+  }
+
+  function squareClick(square){
+    possiblesMoves(square);
+  }
+
+  function pieceDragBegin(piece, square){
+    possiblesMoves(square);
   }
 
   return (
     <div>
       <Chessboard
         position={game.fen()}
-        // onPieceClick={test2}
-        // onDragOverSquare={test2}
+        areArrowsAllowed={false}
         onPieceDrop={onDrop}
-      />;
+        showErrors={'console'}
+        useAnimation={true}
+        onSquareClick={squareClick}
+        onPieceDragBegin={pieceDragBegin}
+        customSquareStyles={squaresStyles}
+      />
     </div>)
 }
 
