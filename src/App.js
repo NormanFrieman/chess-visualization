e04ab4b46e79ad9a3aa4ff8cdcc474e9e9997034
Norman Frieman <ezequiel-d.s@hotmail.com>
2022-06-02 10:44:49 -0300

    feat: reproduzir som ao fazer um movimento válido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js';
+import { useState } from 'react';
+import useSound from 'use-sound';
+import move_sound from './assets/move-sound.mp3';
 
 function App() {
+  const [game, setGame] = useState(new Chess());
+  const [moveSound] = useSound(move_sound);
+
+  function safeMove(modify) {
+    setGame(g => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    })
+  }
+
+  function test2(piece){
+    console.log(piece);
+    console.log("CLICOU");
+  }
+
+  function onDrop(sourceSquare, targetSquare) {
+    let move = null;
+    console.log(sourceSquare + " " + targetSquare);
+    safeMove(game => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q'
+      });
+    })
+
+    if(move === null)
+      return false;
+    
+    moveSound();
+    return true;
+  }
+
   return (
     <div>
       <Chessboard
-        draggable={true}
-        position={(new Chess()).fen()}
-        moveSpeed='slow'
-        snapbackSpeed={500}
-        snapSpeed={100}
+        position={game.fen()}
+        // onPieceClick={test2}
+        // onDragOverSquare={test2}
+        onPieceDrop={onDrop}
       />;
     </div>)
 }
