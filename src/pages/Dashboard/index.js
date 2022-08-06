339a80693fa4dd86b8865903906b3c4aa26610f4
Norman Frieman <ezequiel-d.s@hotmail.com>
2022-08-06 11:35:40 -0300

    feat: implementar botão para exibir e esconder peças

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,20 @@
 import ChessComponent from "../../components/Chess";
+import { Eye, EyeOff } from 'react-feather';
 import './style.css';
+import { useState } from "react";
 
 function Dashboard() {
+    const [showPieces, setShowPieces] = useState(true)
+
     return (
         <div className="container">
             <div className="chessgame">
-                <ChessComponent/>
+                <ChessComponent showPieces={showPieces}/>
             </div>
+            <button className="btn_display_pieces" onClick={() => setShowPieces(!showPieces)}>
+                <Eye className={ showPieces ? 'img_eye_off' : 'img_eye' } color="#FFFFFF"/>
+                <EyeOff className={ showPieces ? 'img_eye' : 'img_eye_off' } color="#FFFFFF"/>
+            </button>
         </div>
     )
 }
