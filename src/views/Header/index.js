d1d393e849c370a69ea145268a991f3d1229004b
Norman Frieman <ezequiel-d.s@hotmail.com>
2022-08-10 10:35:17 -0300

    fix: corrigir indentação header

--- src/views/Header/index.js
+++ src/views/Header/index.js
@@ -1,20 +1,26 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import WebFont from 'webfontloader';
 import './style.css';
 
 function Header() {
+    const navigate = useNavigate();
     useEffect(() => {
         WebFont.load({
-          google: {
-            families: ['Noto Sans']
-          }
+            google: {
+                families: ['Noto Sans']
+            }
         });
-       }, []);
+    }, []);
     
+    function navigateHome(){
+        navigate('/')
+    }
+
     return (
         <header>
           <div className="header">
-            <h1 className="font-face-ns">chess visualization</h1>
+            <h1 onClick={event => navigateHome()} className="font-face-ns">chess visualization</h1>
           </div>
         </header>
     )
